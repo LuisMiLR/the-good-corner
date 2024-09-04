858d3d15f113c2d00a772c846853f68a33c9ada3
LuisMiLR <luismiguel.robles.lr@gmail.com>
2024-09-04 22:23:07 +0200

    add volume at backend

--- backend/src/graphql-resolvers/AdQueries.ts
+++ backend/src/graphql-resolvers/AdQueries.ts
@@ -1,31 +1,29 @@
-import { FieldResolver, Query, Resolver, Root } from "type-graphql";
-import { Ad } from "../entities/Ad";
-import DataLoader from "dataloader";
-import { Tag } from "../entities/Tag";
-import { In } from "typeorm";
+import { FieldResolver, Query, Resolver, Root } from 'type-graphql';
+import { Ad } from '../entities/Ad';
+import DataLoader from 'dataloader';
+import { Tag } from '../entities/Tag';
+import { In } from 'typeorm';
 
-const tagsDataLoader = new DataLoader((ids) => {
-    return Tag.findBy({
-        id: In(ids)
-    });
+const tagsDataLoader = new DataLoader(ids => {
+  return Tag.findBy({
+    id: In(ids),
+  });
 });
 
 @Resolver(Ad)
 export class AdQueries {
-
-    @FieldResolver()
-    async tags(@Root() ad: Ad): Promise<(Tag | Error)[]> {
-        if (ad.tagIds == null || ad.tagIds.length == 0) {
-            return [];
-        }
-        return tagsDataLoader.loadMany(ad.tagIds);
+  @FieldResolver()
+  async tags(@Root() ad: Ad): Promise<(Tag | Error)[]> {
+    if (ad.tagIds == null || ad.tagIds.length == 0) {
+      return [];
     }
+    return tagsDataLoader.loadMany(ad.tagIds);
+  }
 
-    @Query(type => [Ad])
-    async getAllAds(): Promise<Ad[]> {
-        console.log("getAllAds Query called from graphql")
-        const ads: Ad[] = await Ad.find({});
-        return ads;
-    }
-
+  @Query(type => [Ad])
+  async getAllAds(): Promise<Ad[]> {
+    console.log('getAllAds Query called from graphql');
+    const ads: Ad[] = await Ad.find({});
+    return ads;
+  }
 }
