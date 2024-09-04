8ac7f03c9521f7670c7b6f4e6e46900bc6eef1f1
LuisMiLR <luismiguel.robles.lr@gmail.com>
2024-09-04 22:55:25 +0200

    front is dockerised

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,5 +1,5 @@
-import React from "react";
+import React from 'react';
 
 export default function about() {
-  return <p>this project was made with purposes.</p>;
+  return <p>The good corner project.</p>;
 }
